14fdb0c0e2624a8343bcf6f45a871d576e4a8dc8
Misael Lima <misael.lima21@gmail.com>
2021-12-15 20:41:46 -0300

    Created column of user entity

--- src/module/user/entities/user.entity.ts
+++ src/module/user/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('user')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 100})
+  name: string;
+
+  @Column({ length: 100})
+  email: string;
+
+  @Column({ length: 100})
+  password: string;
+
+  @Column()
+  isActive: boolean;
+}
